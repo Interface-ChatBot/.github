eac4cf3260787259879f4fb2aae8ae91af243583
GBLY327 <kimgunmin77@gmail.com>
2022-08-13 11:23:03 +0900

    폴더 및 파일 추가

---
+++ 김건민/test1.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import mysql_user_info
+import pymysql
+
+user = mysql_user_info.user_info
+db = pymysql.connect(db=user['db'], host=user['host'], user=user['user'], passwd=user['passwd'], port=user['port'], charset=user['charset'])
+#cursor = db.cursor()
+
+app = Flask(__name__)
+@app.route("/hello")
+def hello():
+    return "<h1>Hello Flask</h1>"
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+#db.close()
