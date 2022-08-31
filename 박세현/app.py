9737c3429057350c86ae38aecefab1609c91d45d
hyuniverse <89325462+hyuniverse@users.noreply.github.com>
2022-08-31 16:04:22 +0900

    개인 스터디(2)
    app.py 업데이트
    개인 스터디 내용 정리(ipynb)

--- 박세현/app.py
+++ 박세현/app.py
@@ -1,5 +1,5 @@
 #import flask
-from flask import Flask
+from flask import Flask, request, jsonify
 
 #시작: 모듈명을 __name__으로 넘기고 app이라는 객체 생성
 app = Flask(__name__)
@@ -10,10 +10,32 @@ app = Flask(__name__)
 def hello():
     return 'Hello Flask'
 
-@app.route('/login')
-def login():
-    return 'Login'
+@app.route('/animal', methods=['POST'])
+def animal():
+    req = request.get_json()
+    
+    #터미널에서 전달받은 json 내용 출력/확인(삭제 예정)
+    print(req)
+    
+    animal_type = req["action"]["detailParams"]["Animal_type"]["value"]	# json파일 읽기
+    answer = animal_type
+    
+    # 답변 텍스트 설정
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer
+                    }
+                }
+            ]
+        }
+    }
 
-if __name__ == '__main__':
-    #flask앱 실행 
-    app.run()
+    # 답변 전송
+    return jsonify(res)
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=8000, threaded=True)
