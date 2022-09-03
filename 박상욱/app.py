9e4c1b3861104290dd85c54080ec4a0f76a8f0fb
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-09-03 20:01:04 +0900

    Update app.py

--- 박상욱/app.py
+++ 박상욱/app.py
@@ -5,16 +5,58 @@ app = Flask(__name__)
 CORS(app)
 
 #url
-@app.route('/')
+@app.route('/',methods=['GET')
 def index():
+    return 'Hello Flask'
+
+#url
+@app.route('/',methods=['POST')
+def index():
+    content = request.get_json()
+    content = content['userRequest']['utterance']
+
+    content = content.replace("\n","")
+    print(content)
+
+    if content = u"오늘의 메뉴"
+        dataSend = {
+            "version" : "2.0",
+            "template" : {
+                "outputs" : [
+                    {
+                        "simpleText" : {
+                            "text" : "테스트입니다."
+                        }
+                    }
+                ]
+            }
+        }
+    else:
+        
     return 'Hello Flask'
 
 # membership fee
 @app.route('/fee')
 def fee():
+    if method == "POST":
+        # payloads = request.get_json()
+        # pprint.pprint(payloads)
+        _ret = {"version": "2.0",
+                "template": {"outputs": [{"simpleText": {"text": "hello"}}]
+                                }
+                }
+
+    return jsonify(_ret), 200
+
+    
+    # return { "freshman": "15000","nofreshman": "10000"}
+
+# Club Wi-Fi password
+@app.route('/wifi')
+def wifi():
     return {
-        "freshman": "15000",
-        "not freshman": "10000"
+        "interface518": "518interface",
+        "interface518 5G": "518interface"
     }
 
 @app.route('/login')
