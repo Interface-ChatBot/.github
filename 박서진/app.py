dd0823b482fb3c5c594f4b85211ddbae1a38b541
Parkseojin2001 <seojin10912@naver.com>
2022-09-03 18:54:02 +0900

    Update app.py

--- 박서진/app.py
+++ 박서진/app.py
@@ -1,13 +1,35 @@
 from flask import Flask
+from flask_cors import CORS
 app = Flask(__name__)
 
+#URL
 @app.route('/')
 def hello():
     return 'Hello Flask'
+
+# Information on the number of people Interface members
+@app.route('/people')
+def people():
+    data =  {
+        "30th": "10",
+        "31st": "20",
+        "32nd": "34",
+        "33rd": "45",
+        "34th": "50",
+        "35th": "70"
+    }
+    return data
+    
+# Interface introduction
+@app.route('/introduction')
+def introduction():
+    return {
+        "Founding Date": "1989.08"
+    }
 
 @app.route('/login')
 def login():
     return 'Login'
 
 if __name__ == '__main__':
-    app.run() 
+    app.run()
