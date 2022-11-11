718806f0ca8cea84739b0e349e780b026dba2547
GBLY327 <kimgunmin77@gmail.com>
2022-11-12 00:05:22 +0900

    DB uproad

---
+++ 김건민/DB/interface_chatbot_executives.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE  IF NOT EXISTS `interface_chatbot` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `interface_chatbot`;
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: interface_chatbot
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `executives`
+--
+
+DROP TABLE IF EXISTS `executives`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `executives` (
+  `name` varchar(10) NOT NULL,
+  `position` varchar(10) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `executives`
+--
+
+LOCK TABLES `executives` WRITE;
+/*!40000 ALTER TABLE `executives` DISABLE KEYS */;
+INSERT INTO `executives` VALUES ('공민성','부기장','unknown@naver.com'),('권하윤','학술차장','unknown@naver.com'),('동기창','소통','unknown@naver.com'),('류국봉','회장','unknown@naver.com'),('박상욱','고문','dkxkqkrtkddn@naver.com'),('손재호','기획차장','unknown@naver.com'),('이규리','학술부장','qyul2058@gmail.com'),('이승언','기획부장','unknown@naver.com'),('임영변','총무','dudqlsquseo@naver.com'),('홍지섭','기장','ghdwltjq5749@naver.com');
+/*!40000 ALTER TABLE `executives` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-11 22:48:43
