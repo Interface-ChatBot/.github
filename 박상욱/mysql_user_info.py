7a455cc7fc7ffd2670771ba8283f92aa6503537e
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-11-12 04:32:21 +0900

    Modify: IP 변경

--- 박상욱/mysql_user_info.py
+++ 박상욱/mysql_user_info.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-user_info = {'db' : 'interface_chatbot', 'host' : '127.0.0.1', 'user' : 'root', 'passwd' : '', 'port' : 3306, 'charset' : 'utf8'}
+user_info = {'db' : 'interface_chatbot', 'host' : 'localhost', 'user' : 'root', 'passwd' : '', 'port' : 3306, 'charset' : 'utf8'}
