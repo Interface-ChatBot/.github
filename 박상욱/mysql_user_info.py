a621e4041f3505e9b7a9a26e22e6dfeb7b9026b2
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-11-12 04:21:45 +0900

    UTF-8 파일 수정

--- 박상욱/mysql_user_info.py
+++ 박상욱/mysql_user_info.py
@@ -1,1 +1,2 @@
-user_info = {'db' : 'interface_chatbot', 'host' : '127.0.0.1', 'user' : 'root', 'passwd' : '11111111', 'port' : 3306, 'charset' : 'utf8'}
+# -*- coding: utf-8 -*-
+user_info = {'db' : 'interface_chatbot', 'host' : '127.0.0.1', 'user' : 'root', 'passwd' : '', 'port' : 3306, 'charset' : 'utf8'}
