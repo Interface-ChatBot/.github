f38c0612c4d179daf1d2001d26c111eb507332a7
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-11-15 15:59:12 +0900

    Test: import용 res.py 파일 추가
    import용 res.py 파일 추가

---
+++ 박상욱/sehyunT/res.py
@@ -0,0 +1,17 @@
+# -*- coding: utf8 -*-
+
+def RES(text):
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": text
+                    }
+                }
+            ]
+        }
+    }
+    return res
+
