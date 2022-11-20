03cf4b723a2771987848b02c1019a8ffdad3ba55
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-11-20 21:32:00 +0900

    Feat: 기능팀 코드 추가 및 창립제 발표 자료 추가
    기능팀 집부 소개 기능 추가 및 창립제 발표 자료 추가

--- 박상욱/app.py
+++ 박상욱/app.py
@@ -201,7 +201,66 @@ def password():
             ]
         }
     }
+
+#인터페이스 링크 안내
+@application.route("/interface_link", methods=['POST'])
+def interface_link():
+    req = request.get_json()
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "인스타그램:https://www.instagram.com/interface518/"
+                                "페이스북:https://www.instagram.com/interface518/"  # 인스타
+                                "홈페이지:https://sejong-interface.github.io/"
+                                "깃허브: https://github.com/sejonginterface"
+                                "메일:518interface@gmail.com"
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(res)
+
+@application.route("/executive_member", methods=['POST'])
+def executive_member():
+    req = request.get_json()
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "회장:류국봉 wntlghks0107@naver.com\n고문: 박상욱 dkxkqkrtkddn@naver.com\n총무:임영빈 wlso@naver.com"
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(res)
+
+# Interface introduction
+@application.route("/introduction",methods = ['POST'])
+def introduction():
+    req = request.get_json()
     
+    text="인터페이스 소개\n" + "동아리 연혁 : 1988년\n"+ "주요 활동\n1. 동아리 자체 대회/ 전시회\n2. 다양한 스터디\n3. 소모임 활동"
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": text
+                    }
+                }
+            ]
+        }
+    }
+    
+    return jsonify(res)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=5000, threaded=True)
