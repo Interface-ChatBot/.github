552aec53530df044afbefad543019be9861467df
hyuniverse <89325462+hyuniverse@users.noreply.github.com>
2022-11-28 15:32:21 +0900

    Update: DB, 기능
    db 및 기능 추가

--- 박세현/main.py
+++ 박세현/main.py
@@ -1,7 +1,8 @@
-# -*- coding: utf8 -*-
-
-from flask import Flask, request, jsonify
+# -*- coding: utf-8 -*-
+from flask import Flask,request,jsonify
+from interface_db import *
 from res import RES
+
 
 application = Flask(__name__)
 
@@ -11,30 +12,45 @@ def hello():
     return "Hello goorm!" #구름 서버 사용
 
 
-
 # 동아리 회비 안내
 @application.route("/fee",methods=['POST'])
-def fee():
-    # 신입생인지 재학생인지 request로 구분
+def _fee():
     
     req = request.get_json()
 
+    userRes = req["userRequest"]["utterance"]	 				
+    member_type = req["action"]["clientExtra"]["member_type"]	
     
-    userRes = req["userRequest"]["utterance"]	 				#사용자 발화 저장
-    member_type = req["action"]["clientExtra"]["member_type"]	#바로가기 parameter 저장
-        
-    fee = 0
-    
-    if member_type == "신입생":
+    print(userRes)
+    print(member_type)
+
+    data = fee()
+
+    club_fee = 0
+    m_type = ""
+
+
+    if data[0]["type"] == member_type:
+        club_fee = data[0]["fee"]
+        m_type = "신입생"
+        #data[0]["type"].encoding("utf-8)
+    elif data[1]["type"] == member_type:
+        club_fee = data[1]["fee"]
+        m_type = "재학생"
+        #str(data[1]["type"])
+
+    '''
+    if member_type == "재학생":
         fee = 20000
-    elif member_type == "재학생":
+    elif member_type == "신입생":
         fee = 20000
-    
-    text = "[동아리 회비]\n" + member_type + " : " + str(fee) + "원"
-    
+    '''
+
+    text = "동아리 회비 안내" + "\n" + m_type + " : " + str(club_fee) + "원"
     res = RES(text)
-    
+
     return jsonify(res)
+
 
 # Wi-Fi 비밀번호 안내
 @application.route("/wifi",methods=['POST'])
@@ -49,6 +65,24 @@ def wifi():
     
     return jsonify(res)
 
+'''
+//다른 와이파이 방식인데, 이것도 굳이 데베에서 불러와야하나.
+@application.route("/wifi",methods=['POST'])
+def _wifi():
+    
+    data = wifi()
+    str = ""
+    for i in data:
+        str += "name : " + i["name"] + " pw : " + i["pw"]
+        str += "\n"
+
+    text=str
+    res = RES(text)
+
+    return jsonify(res)
+
+'''
+
 
 # 동아리 재실 / 퇴실 여부 체크
 @application.route("/isroom",methods=['POST'])
@@ -56,26 +90,40 @@ def isroom():
     
     req = request.get_json()
     
-    userRes = req["userRequest"]["utterance"]	 #사용자 발화 저장
-    
-    pnum = 0	#DB에서 가져오기
+    #userRes = req["userRequest"]["utterance"]
+    room_type = req["action"]["clientExtra"]["room_type"]	    
+    print(room_type)
+    pnum = 0	
     text = ""
     
-    if userRes == "재실":
-        # DB 재실인원 증가
-        pnum += 1
+    if room_type.encode('utf-8') == "재실":
+        # DB 
+        mic_plus()
         
-    elif userRes == "퇴실":
-        # DB 재실인원 감소
-        pnum += -1
+    elif room_type.encode('utf-8') == "퇴실":
+        # DB 
+        mic_minus()
             
-    text = "현재 재실 인원은 " + str(pnum) + "명 입니다."
+    data = mic_show()
     
-    # 6시에 재실인원 0으로 초기화
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        #"text": text
+                        "text": "현재 동아리원 : " + str(data)
+                    }
+                }
+            ]
+        }
+    }
     
-    
+    print(res)
+
     res = RES(text)
-        
+
     return jsonify(res)
 
 
@@ -93,25 +141,93 @@ def peoplenum():
 
 
 
+#현재 동아리 인원
+@application.route("/peoplenum",methods=['POST'])
+def peoplenum():
+    
+    pnum = mic_show()
+    
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "현재 동아리원 : " + str(pnum)
+                    }
+                }
+            ]
+        }
+    }
+        
+    res = RES(text)
+
+    return jsonify(res)
+
+
+
 # 동아리방 위치 안내
 @application.route("/clubroom",methods=['POST'])
 def clubroom():
-    
-    text = "동아리방 위치 : 세종대학교 학생회관 518호\nhttps://naver.me/F6mxi8mf"
-    
-    res = RES(text)
+    '''
+    data = location()// 굳이 불러와야 하나..?
+
+    str = data[1]["type"] + " : " + data[1]["adress"] + "\n" + data[0]["type"] + " : " + data[0]["adress"]
+    '''
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+		    "basicCard":  {
+                        "description": "서울특별시 광진구 능동로 209 세종대학교 학생회관 518호",
+                        "buttons": [
+                            {
+                                "action": "webLink",
+                                "label": "네이버 지도 바로가기",
+                                "webLinkUrl": "https://naver.me/F6mxi8mf"
+                            }
+                        ]
+                    }   
+                }
+            ]
+        }
+    }
     
     return jsonify(res)
 
 
-# Interface introduction
+'''
+#동방 비번 (주소 바꿔야함. 겹침.)
+@application.route("/clubroom",methods=['POST'])
+def password():
+
+    #학번 이름
+    #22000000 김인페
+    userRes = req["userRequest"]["utterance"]
+
+    data = member_check(userRes.split(' '))
+
+    str = ""
+
+    if data==1:
+        str = "동아리 비밀번호 : " + "7585"
+    else:
+        str = "정보가 일치하지 않습니다" + "\n" + "다시 입력하거나 집부에게 문의 부탁드립니다."
+
+    res = RES(str)
+
+    return jsonify(res)
+'''
+
+# 인터페이스 소개
 @application.route("/introduction",methods = ['POST'])
 def introduction():
     req = request.get_json()
     
     text="인터페이스 소개\n" + "동아리 연혁 : 1988년\n"+ "주요 활동\n1. 동아리 자체 대회/ 전시회\n2. 다양한 스터디\n3. 소모임 활동"
     res = RES(text)
-    
+
     return jsonify(res)
     
 
@@ -225,16 +341,16 @@ def suggestion():
     
     return jsonify(res)
 
+
 #인터페이스 집부 구성원 안내
 @application.route("/executive_member", methods=['POST'])
 def executive_member():
     req = request.get_json()
-
-    text = "회장:아무개 wntlghks0107@naver.com\n부회장:이무개 wntis0107@naver.com\n총무:길동 wlso@naver.com"
-
+    text="회장:류국봉 wntlghks0107@naver.com\n고문: 박상욱 dkxkqkrtkddn@naver.com\n총무:임영빈 wlso@naver.com"
     res = RES(text)
-    
+
     return jsonify(res)
+
 
 #인터페이스 링크 안내
 @application.route("/interface_link", methods=['POST'])
@@ -250,6 +366,7 @@ def interface_link():
     res = RES(text)
 
     return jsonify(res)
+
 
 #인터페이스 행사 소개
 @application.route("/event", methods=['POST'])
@@ -302,6 +419,7 @@ def event():
 
          ]}}
     return jsonify(res)
+
 
 
 if __name__ == "__main__":
