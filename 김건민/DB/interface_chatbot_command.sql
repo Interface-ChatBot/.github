c30ddeffe0a056a20b7e7a6485fea124de27c61e
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2022-11-28 20:49:23 +0900

    Fix: SQL 오류 수정

--- 김건민/DB/interface_chatbot_command.sql
+++ 김건민/DB/interface_chatbot_command.sql
@@ -27,7 +27,7 @@ DROP TABLE IF EXISTS `command`;
 CREATE TABLE `command` (
   `command` varchar(100) DEFAULT NULL,
   `example` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
